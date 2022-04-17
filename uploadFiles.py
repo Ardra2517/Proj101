7b69a465e58f19b04ddd0e1bf047dcde52984fd9
Ardra2517 <74819734+Ardra2517@users.noreply.github.com>
2022-04-17 20:23:12 +0530

    Update uploadFiles.py

--- uploadFiles.py
+++ uploadFiles.py
@@ -11,4 +11,12 @@ class uploadFiles:
         f = open(file_from, 'rb') 
         dbx.files_upload(f.read(), file_to)
 
- 
+ def main():
+    access_token = 'sl.BF4xs5orT9rnSzWZYPtrizamw2fjTQhvE5G5TgDsCNx81I5nWDiTglSUnAbxKhECV7EWYNqNMz-EGS20lpxHAn0q1XRxGgqsqZLnGXmg0VPryekPChhOQkXFhBVR8O8g_-tTEuY'
+    transferData = TransferData(access_token)
+
+    file_from=input('Enter the file path to transfer')
+    file_to=input('Enter the destination path')
+
+    transferData.upload_file(file_from, file_to)
+    print("File has been moved.")
